d912ac7f223019695255fba6b5d8c4c8449e8025
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-01-30 12:04:23 +0600

    make server running

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(
+	cors({
+		origin: process.env.APP_URL!,
+		credentials: true
+	})
+)
+
+app.get('/', (req, res) => {
+	res.send('Tutor Link Server is Running')
+})
+
+export default app
