468a4db68daf356a701c10d8c7c979209db7afca
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-01-31 21:22:55 +0600

    create a async handler function to handle controller error

---
+++ src/lib/asyncHandler.ts
@@ -0,0 +1,13 @@
+import type { Request, Response, NextFunction } from 'express'
+
+type AsyncHandlerFunction = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>
+
+export const asyncHandler = (fn: AsyncHandlerFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next)
+  }
+}
