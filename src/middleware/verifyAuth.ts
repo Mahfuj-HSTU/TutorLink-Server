72fee903564e99387c537edb4beed294ff8ad290
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-02-02 13:56:17 +0600

    update user role type and tutor schema

--- src/middleware/verifyAuth.ts
+++ src/middleware/verifyAuth.ts
@@ -2,34 +2,35 @@ import type { NextFunction, Request, Response } from 'express'
 import { auth as betterAuth } from '../lib/auth.ts'
 
 export enum UserRole {
-  USER = 'USER',
-  ADMIN = 'ADMIN'
+	STUDENT = 'STUDENT',
+	TUTOR = 'TUTOR',
+	ADMIN = 'ADMIN'
 }
 
 export const verifyAuth = (...roles: UserRole[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const session = await betterAuth.api.getSession({
-        headers: req.headers as any
-      })
-      if (!session?.user) {
-        return res.status(401).json({ message: 'Unauthorized' })
-      }
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const session = await betterAuth.api.getSession({
+				headers: req.headers as any
+			})
+			if (!session?.user) {
+				return res.status(401).json({ message: 'Unauthorized' })
+			}
 
-      req.user = {
-        id: session.user.id,
-        email: session.user.email,
-        name: session.user.name,
-        role: session.user.role as string
-      }
-      if (roles.length && !roles.includes(req.user.role as UserRole)) {
-        return res
-          .status(401)
-          .json({ message: 'You are not authorized to perform this action' })
-      }
-      next()
-    } catch (error) {
-      next(error)
-    }
-  }
+			req.user = {
+				id: session.user.id,
+				email: session.user.email,
+				name: session.user.name,
+				role: session.user.role as string
+			}
+			if (roles.length && !roles.includes(req.user.role as UserRole)) {
+				return res
+					.status(401)
+					.json({ message: 'You are not authorized to perform this action' })
+			}
+			next()
+		} catch (error) {
+			next(error)
+		}
+	}
 }
